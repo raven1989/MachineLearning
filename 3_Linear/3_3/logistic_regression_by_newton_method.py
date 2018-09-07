f14900be0c06dac09274b7dd90828d65f6035306
raven1989 <okuma0513@yahoo.com>
2018-09-07 19:02:42 +0800

    Kernel logistic regression DONE.

--- 3_Linear/3_3/logistic_regression_by_newton_method.py
+++ 3_Linear/3_3/logistic_regression_by_newton_method.py
@@ -1,3 +1,4 @@
+#! coding:utf8
 import sys
 import math
 import numpy as np
@@ -14,11 +15,13 @@ def log_likelihood_first_second_derivatives(w, b, x, y):
   p1 = p_y_equal_1(x, w, b)
   y_minus_p1 = y-p1
   # print("p1:{} y_minus_p1:{}".format(p1, y_minus_p1))
-  ## [1x2, scalar]
-  first_derivative = [-np.dot(np.transpose(y_minus_p1), x), -np.sum(y_minus_p1)]
+  ## [1 x n, scalar]
+  first_derivative = [-np.sum(y_minus_p1*x, axis=0), -np.sum(y_minus_p1)]
   # print("first_derivative:{}".format(first_derivative))
-  ## scalar
-  second_derivative = np.sum(np.dot(x,x.T)*p1*(1.0-p1))
+  # print((np.dot(x,x.T)*p1*(1.0-p1)).shape)
+  ## [n x n, scalar]
+  ### 高维下的二阶矩阵就是海森矩阵，维度是nxn的
+  second_derivative = [np.dot(x.T, x*p1*(1.0-p1)), np.sum(p1*(1.0-p1), axis=0)]
   return first_derivative, second_derivative
 
 def do_predict(x, y, w, b):
@@ -44,12 +47,15 @@ if __name__ == "__main__":
 
   ## update w and b
   delta = lambda x1,x2:np.linalg.norm(x1-x2) 
-  epsilon = 1e-4
+  epsilon = 1e-10
   epoch = 0
   while True:
     first_derivative, second_derivative = log_likelihood_first_second_derivatives(w, b, x, y)
     tmp_w, tmp_b = w, b
-    w, b = [tmp_w, tmp_b] - first_derivative/second_derivative
+    # w, b = [tmp_w, tmp_b] - first_derivative/second_derivative
+    ### 高维下，first_derivative/second_derivative = second_derivative的逆 dot first_derivative
+    w = w - np.dot(np.linalg.inv(second_derivative[0]), first_derivative[0])
+    b = b - first_derivative[1]/second_derivative[1]
     if epoch%100==0:
       error = delta(w, tmp_w)
       predictions = do_predict(x, y, w, b)
--- 6_SupportVectorMachine/6_9/kernel_logistic_regression_by_newton_method.py
+++ 6_SupportVectorMachine/6_9/kernel_logistic_regression_by_newton_method.py
@@ -1,3 +1,4 @@
+#! coding:utf8
 import sys
 reload(sys)
 sys.setdefaultencoding('utf8')
@@ -19,16 +20,20 @@ def p_1(beta, x):
 
 def log_likelihood_first_second_derivatives(beta, x, y):
   p1 = p_1(beta, x)
-  first_derivative = x * (p1-y)
-  second_derivative = np.dot(x.T, x)*p1*(1-p1)
+  first_derivative = np.multiply(x,(p1-y))
+  second_derivative = np.dot(x, x.T*p1*(1-p1))
+  # print("p1:{} first derivative.shape:{} second derivative.shape:{}".format(p1, first_derivative.shape, second_derivative.shape))
   return first_derivative, second_derivative
 
-def gussian_kernel(X, sigma):
+def gussian_kernel(sigma, x1, x2):
+  return np.exp(-1.0/sigma*(np.linalg.norm(x1-x2))**2)
+
+def gussian_kernel_matrix(X, sigma):
   m, n = X.shape
   K = []
   for i in range(m):
     for j in range(m):
-      K.append(np.exp(-1.0/sigma*(np.linalg.norm(X[i]-X[j]))**2))
+      K.append(gussian_kernel(sigma, X[i], X[j]))
   K = np.reshape(K, (m,m))
   return K
 
@@ -36,8 +41,36 @@ def log_likelihood(beta, K, Y):
   m = Y.shape[0]
   l = 0
   for i in range(m):
-    l += -1.0*np.dot(beta.T, K[:,i:i+1]) + np.log(1+np.exp(np.dot(beta.T, K[:,i:i+1].T)))
+    # print(np.dot(beta.T, K[:,i:i+1]))
+    line = np.dot(beta.T, K[:,i:i+1])
+    l += -1.0*Y[i]*line + np.log(1.0+np.e**line)
+    # print("line:{} l:{}".format(line, l))
   return l
+
+def reference(beta, sigma, X, x):
+  m, _ = X.shape
+  num, _ = x.shape
+  print("X.shape:{} x.shape:{}".format(X.shape, x.shape))
+  pres = []
+  for j in range(num):
+    p = []
+    for i in range(m):
+      p.append(gussian_kernel(sigma, x[j:j+1,:], X[i:i+1,:]))
+    p.append(1)
+    p = np.reshape(p, (m+1,-1))
+    p = sigmoid(np.dot(beta.T, p))
+    pres.append(p)
+  return np.reshape(pres, (x.shape[0],1))
+
+def predict(beta, sigma, X, x):
+  pre = reference(beta, sigma, X, x)>0.5
+  return np.array(pre, dtype=float)
+
+def cal_accuracy(predictions, true_ys):
+  pre = np.reshape(predictions, true_ys.shape)
+  correct = np.equal(true_ys, pre)
+  accuracy = np.sum([1 if x else 0 for x in correct])*1.0/len(correct)
+  return accuracy
 
 if __name__=='__main__':
   csv = ROOT_DIR+'/../../Data/watermelon/watermelon_3.0_x.csv'
@@ -50,30 +83,45 @@ if __name__=='__main__':
   epsilon = 1e-6
 
   ### parameters
-  alpha = np.random.normal(0, 0.1, (X.shape[0],1))
-  b = np.random.normal(0, 0.1)
+  # alpha = np.random.normal(0, 0.1, (X.shape[0],1))
+  # b = np.random.normal(0, 0.1)
 
-  beta = np.reshape(np.append(alpha, b), (1,-1))
+  alpha = np.zeros((X.shape[0],1))
+  b = 1
 
-  K = gussian_kernel(X, sigma)
+  beta = np.reshape(np.append(alpha, b), (alpha.shape[0]+1, -1))
+  print("Parameter alpha.shape:{} beta.shape:{}".format(alpha.shape, beta.shape))
+
+  K = gussian_kernel_matrix(X, sigma)
   K = np.row_stack((K, np.ones((1,K.shape[1]))))
   K = np.column_stack((K, np.ones((K.shape[0],1))))
+  # print("Kernel K:{}".format(K))
 
   m, _ = X.shape
   epoch = 0
+  l = 0
   while True:
-    beta_epoch = np.zeros(beta.shape)
+    first = 0;
+    second = 0;
     for i in range(m):
-      first_derivative, second_derivative = log_likelihood_first_second_derivatives(beta, K[:,i:i+1].T, Y[i])
-      beta_epoch = beta_epoch + second_derivative/first_derivative
-    beta = beta - beta_epoch
-    l = log_likelihood(beta, K, Y)
-    if epoch%100==0:
-      print("Epoch:{} log likelihood:{}".format(epoch, l))
-    if l<epsilon:
+      first_derivative, second_derivative = log_likelihood_first_second_derivatives(beta, K[:,i:i+1], Y[i])
+      first = first + first_derivative
+      second = second + second_derivative
+    # print("beta:{} first:{} second:{}".format(beta.shape, first.shape, second.shape))
+    ### 高维下，二阶导数是海森矩阵，相应的更新公式是 beta(t+1) = beta(t) - H.inv dot first_derivative
+    beta = beta - np.dot(np.linalg.inv(second), first)
+    cur_l = log_likelihood(beta, K, Y)
+    if epoch%1==0:
+      print("Epoch:{} log likelihood:{}".format(epoch, cur_l))
+    if np.absolute(l-cur_l)<epsilon:
       break
+    l = cur_l
     epoch += 1
   l = log_likelihood(beta, K, Y)
-  print("Epoch:{} log likelihood:{}".format(epoch, l))
-
+  print("End log likelihood:{}".format(l))
+  ### predict
+  predictions = predict(beta, sigma, X, X)
+  print("Predictions:{} Y:{}".format(predictions, Y))
+  acc = cal_accuracy(predictions, Y)
+  print("Accuracy:{}".format(acc))
 

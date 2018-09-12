9cd2764b5aefea0c6d9a087f84050a7a22c92afd
raven1989 <okuma0513@yahoo.com>
2018-09-13 00:40:42 +0800

    aode finished

--- 7_Bayes/7.6/aode_laplacian_bayes.py
+++ 7_Bayes/7.6/aode_laplacian_bayes.py
@@ -31,6 +31,7 @@ num_class = len(feature_maker.feature2index[-1])
 ### 离散特征
 ### 特征的属性数量总和
 num_property = discrete_X.shape[1]
+print("sample num:{} class_num:{} ".format(num_sample, num_class))
 ### 属性矩阵 num_class x num_property x num_property
 ### 每一个元素N(c,i,j) 表示 (第c类 & 特征fa取值为i & 特征fb取值为j)
 N_shape = (num_class, num_property, num_property)
@@ -45,7 +46,7 @@ for i,x in enumerate(discrete_X):
   # print("c:{} x:{}".format(c, np.dot(x, x.T)))
   N[c] += np.dot(x, x.T)
   ### 将对角线置为0，因为对角线是属性自己计数，只要属性i存在则，N(c,i,i)必为1
-  np.fill_diagonal(N[c], np.zeros(num_property))
+  # np.fill_diagonal(N[c], np.zeros(num_property))
 print("N:{}".format(N))
 
 ### 连续特征
@@ -68,6 +69,7 @@ test = [["青绿", "蜷缩", "浊响", "清晰", "凹陷", "硬滑", 0.697, 0.46
 print("test:{}".format(json.dumps(test, ensure_ascii=False)))
 
 test_X = feature_maker.encode(X=test, one_hot=True)
+# test_X = X
 print("encoded test:{}".format(test_X))
 
 test_discrete = test_X[:,:-num_continuous_feature]
@@ -78,41 +80,50 @@ threshold_m = 0
 
 pre = np.zeros((test_X.shape[0], num_class))
 for k,x in enumerate(test_discrete):
-  print("k:{} x:{}".format(k, x))
+  # print("k:{} x:{}".format(k, x))
   xx = np.reshape(x, (x.shape[0],1))
   N_mask = np.dot(xx, xx.T)
   # print(N_mask)
   for c in range(num_class):
     N_c = np.multiply(N[c], N_mask)
     # print("N_{}:{}".format(c, N_c))
+    p = 1.0
     for i,fi in enumerate(x):
-      if fi==1:
+      if fi>0:
         #############################
         ### 先计算先验概率P(c,fi) ###
         #############################
         ### label为c并且特征f取值为i的个数
-        d_fi = np.sum(N_c[i])
+        d_fi = N_c[i][i]
         if d_fi<threshold_m:
           continue
         ### 特征f的属性个数
-        f_end = bisect.bisect_right(feature_partition, i)
-        num_f_property = feature_partition[f_end]-feature_partition[f_end-1]
+        fi_end = bisect.bisect_right(feature_partition, i)
+        num_f_property = feature_partition[fi_end]-feature_partition[fi_end-1]
         prior_prob = (d_fi+1)*1.0/(num_sample+num_class*num_f_property)
-        prior_prob_log = np.log(prior_prob)
-        pre[k][c] += prior_prob_log
+        p *= prior_prob
+        print("P(c:{}-{},i:{}-{}) = {}  d_fi:{}".format(c, feature_maker.index2feature[-1].get(c),
+                                                        i, feature_maker.index2feature[fi_end-1].get(i-feature_partition[fi_end-1]), 
+                                                        prior_prob, 
+                                                        d_fi))
         #######################################
         ### 再计算条件概率 Σ log P(fj|c,fi) ###
         #######################################
-        print("i:{} N_{}_{}:{}".format(i, c, i, N_c[i]))
         for j,fj in enumerate(N_c[i]):
-          if fj>0:
+          if fj>0 and j!=i:
             d_fj = fj
-            f_end = bisect.bisect_right(feature_partition, j)
-            num_f_property = feature_partition[f_end]-feature_partition[f_end-1]
+            fj_end = bisect.bisect_right(feature_partition, j)
+            num_f_property = feature_partition[fj_end]-feature_partition[fj_end-1]
             cond_prob = (d_fj+1)*1.0/(d_fi+num_f_property)
-            cond_prob_log = np.log(cond_prob)
-            pre[k][c] += cond_prob_log
-print("Predict:{}".format(np.exp(pre)))
+            p *= cond_prob
+            print("P(j:{}-{}|c:{}-{},i:{}-{}) = {}  d_fi:{} d_fj:{}".format(j, feature_maker.index2feature[fj_end-1].get(j-feature_partition[fj_end-1]), 
+                                                               c, feature_maker.index2feature[-1].get(c),
+                                                               i, feature_maker.index2feature[fi_end-1].get(i-feature_partition[fi_end-1]), 
+                                                               cond_prob, 
+                                                               d_fi, d_fj))
+    print("k:{} c:{} p:{}".format(k, c, p))
+    pre[k][c] += p
+print("Predict:{}".format(pre))
 
 # print(test_x)
 # print(json.dumps(feature_maker.decode(test_x, one_hot=True), ensure_ascii=False))

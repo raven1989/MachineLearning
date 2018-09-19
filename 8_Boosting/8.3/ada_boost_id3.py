18f81a9cf212fc144240a0c204d7e805095d0e95
raven1989 <okuma0513@yahoo.com>
2018-09-19 17:04:39 +0800

    ada_boosting finished.

---
+++ 8_Boosting/8.3/ada_boost_id3.py
@@ -0,0 +1,60 @@
+#coding:utf-8
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+import os
+ROOT_DIR = os.path.abspath(os.path.dirname(__file__))
+sys.path.append(ROOT_DIR+"/../../4_DecisionTree/Model")
+sys.path.append(ROOT_DIR+"/../Model")
+from decision_tree_model import DecisionTreeModel
+from ada_boost import WeakClassifier
+from ada_boost import AdaBoostClassifier
+import numpy as np
+import json
+
+class DecisionTreeAsWeakClassifier(WeakClassifier):
+   
+  def __init__(self, model, label_mapping):
+    self.classifier = DecisionTreeModel()
+    self.model = model
+    self.label_mapping = label_mapping
+
+  def fit(self, data, distribution):
+    # print(data)
+    self.classifier.fit(data=data, algo_model=self.model, data_weights=distribution)
+
+  def predict(self, X):
+    pre = self.classifier.predict(X)
+    return np.array([self.label_mapping.get(x) for x in pre])
+
+  def mapping_label(self, Y):
+    return np.array([self.label_mapping.get(x) for x in Y])
+
+
+if __name__ == "__main__":
+  data = np.loadtxt(ROOT_DIR+"/../../4_DecisionTree/watermelon/watermelon_3.0_x.csv", dtype=str, delimiter=",", skiprows=0)
+  # data = np.loadtxt(ROOT_DIR+"/../../4_DecisionTree/watermelon/watermelon_3.0.csv", dtype=str, delimiter=",", skiprows=0)
+  data = data[:,1:]
+
+  num_weak = 10
+  label_map = {"好瓜":1., "坏瓜":-1.}
+  weak_classifiers = [DecisionTreeAsWeakClassifier(model="id3", label_mapping=label_map) for i in range(num_weak)]
+  model = AdaBoostClassifier(weak_classifiers=weak_classifiers)
+
+  model.fit(data, skip_rows=1)
+  # print(json.dumps(model.root, indent=4, ensure_ascii=False))
+
+  ## predict train
+  x = data[1:,:-1]
+  y = data[1:,-1]
+  predictions = model.predict(x)
+  real = weak_classifiers[0].mapping_label(y)
+  print(predictions)
+  print(real)
+  print("Train Accuracy:{}".format(np.mean(predictions==real)))
+
+  dot = None
+  for weaker in model.weak_classifiers:
+    dot = weaker.classifier.export_graphviz(dot)
+  print(dot.render(view=True, cleanup=True))
+
---
+++ 8_Boosting/Model/ada_boost.py
@@ -0,0 +1,58 @@
+#coding:utf-8
+import sys
+import numpy as np
+
+class WeakClassifier:
+  def fit(self, data, distribution):
+    raise NotImpletementError("Abstract method not impletemented")
+  ### 规范化输出1和-1
+  def predict(self, X):
+    raise NotImpletementError("Abstract method not impletemented")
+  def mapping_label(Y):
+    raise NotImpletementError("Abstract method not impletemented")
+
+class AdaBoostClassifier:
+
+  def __init__(self, weak_classifiers):
+    self.num_weaker = 0
+    self.weak_classifiers = weak_classifiers
+    self.alpha = []
+
+  def fit(self, data, skip_rows=0, skip_cols=0):
+    m, n = data.shape
+    m -= skip_rows
+    n -= skip_cols
+    X = data[skip_rows:, skip_cols:-1]
+    Y = data[skip_rows:, -1]
+    ### 准备一个小值，以应对弱分类器错误率为0的情况 
+    epsilon = 1e-10
+    ### 初始化样本分布权值
+    distribution = np.array([1.0/m]*m)
+    # print(distribution)
+    for t in range(len(self.weak_classifiers)):
+      weak_classifier = self.weak_classifiers[t]
+      real = weak_classifier.mapping_label(Y)
+      # print("data:{}".format(data))
+      weak_classifier.fit(data, distribution)
+      pre = weak_classifier.predict(X)
+      error = 1.0-np.mean(pre==real)
+      print("t:{} error:{} detail:{}".format(t, error, pre==real))
+      if error>0.5:
+        # print("Weak classifier {} error:{}, training exit.".format(t, error))
+        break
+      alpha = 0.5*np.log((1.0-error+epsilon)/(error+epsilon))
+      self.alpha.append(alpha)
+      print("t:{} alpha:{}\ndistribution:{}\nd_sum:{}".format(t, self.alpha[t], distribution, np.sum(distribution)))
+      d = [distribution[i]*np.exp(-self.alpha[t]*pre[i]*real[i]) for i in range(m)]
+      # print("d:{}".format(d))
+      z = np.sum(d)
+      distribution = d/z
+    while len(self.weak_classifiers)>len(self.alpha):
+      self.weak_classifiers.pop()
+
+  def predict(self, X):
+    weak_pres = [self.weak_classifiers[i].predict(X)*self.alpha[i] for i in range(len(self.alpha))]
+    # print(weak_pres)
+    return np.sign(np.sum(weak_pres, axis=0))
+
+    
